b5b259870d6b31f9288673ad438cec42a7cceb9b
Siddhant Shaw <105769861+Sid-thebot@users.noreply.github.com>
2024-06-04 23:10:25 +0530

    Create Network Communication Modules.cpp

---
+++ Network Communication Modules.cpp
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(7, 8); // RX, TX
+
+void setup() {
+  mySerial.begin(9600);
+  Serial.begin(9600);
+  delay(1000);
+  mySerial.print("AT+CMGF=1\r");
+  delay(100);
+  mySerial.print("AT+CMGS=\"+1234567890\"\r");
+  delay(100);
+  mySerial.print("Hello from Arduino!\r");
+  delay(100);
+  mySerial.write(26); // ASCII code of CTRL+Z
+}
+
+void loop() {
+}
