f232dbcf6410ad41826278e189b7543b4e830b27
Siddhant Shaw <105769861+Sid-thebot@users.noreply.github.com>
2024-06-07 23:44:05 +0530

    Agriculture.ino

--- Agriculture.ino
+++ Agriculture.ino
@@ -1,7 +1,6 @@
 #define BLYNK_TEMPLATE_ID "***************"
 #define BLYNK_TEMPLATE_NAME "**************"
 #define BLYNK_AUTH_TOKEN "***********************************"
-
 #include <DHT.h>
 #include <Wire.h>
 #include <WiFiClient.h>
